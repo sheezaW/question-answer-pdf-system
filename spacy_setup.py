135184a87aa92533a3ff19cb50db63c56173ac5e
sheezaW <sheezawaheed1234@gmail.com>
2023-09-26 13:28:35 +0500

    Add files via upload

---
+++ spacy_setup.py
@@ -0,0 +1,12 @@
+import spacy
+
+def setup_spacy():
+    # Check if the desired language model is already installed
+    if not spacy.util.is_package("en_core_web_sm"):
+        # Download and install the language model
+        spacy.cli.download("en_core_web_sm")
+        spacy.cli.link("en_core_web_sm", "en", force=True)
+
+# Call the setup function when the script is run
+if __name__ == "__main__":
+    setup_spacy()
