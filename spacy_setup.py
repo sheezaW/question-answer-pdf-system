5af0d2988aa50cd866ed0b7e4e389b13f6e5fc08
sheezaW <sheezawaheed1234@gmail.com>
2023-09-26 13:30:28 +0500

    spacy_setup.py

--- spacy_setup.py
+++ spacy_setup.py
@@ -3,7 +3,7 @@ import spacy
 def setup_spacy():
     # Check if the desired language model is already installed
     if not spacy.util.is_package("en_core_web_sm"):
-        # Download and install the language model
+        # Download and install the language model (without specifying a version)
         spacy.cli.download("en_core_web_sm")
         spacy.cli.link("en_core_web_sm", "en", force=True)
 
