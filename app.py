269cc2d9355d3e5d81f94d2f59f5dde82d2da01a
sheezaW <sheezawaheed1234@gmail.com>
2023-09-26 12:55:58 +0500

    app.py

--- app.py
+++ app.py
@@ -1,29 +1,52 @@
 import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.chains.router import MultiRetrievalQAChain
 from langchain.llms import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.document_loaders import TextLoader
 from langchain.vectorstores import FAISS
+import os
+with st.sidebar:
+    st.title('Document Chat App')
+    st.markdown('''
+    ## About
+    This app is an OpenAI-powered Chatbot built using:
+    - streamlit
+    - langchain
+    - OpenAI
+    '''    
+    )
+    add_vertical_space(5)
+    st.write('Made by Solutyics')
+
 
 # Function to initialize the MultiRetrievalQAChain
 def initialize_qa_chain(document_paths):
     # Initialize an empty list to store document objects
     documents = []
 
-    # Load and split each document
-    for document_path in document_paths:
-        document = TextLoader(document_path).load_and_split()
-        documents.append(document)
-
     # Create retrievers for each document
     retrievers = []
 
-    for document in documents:
-        retriever = FAISS.from_documents(document, OpenAIEmbeddings()).as_retriever()
-        retrievers.append(retriever)
+    for document_path in document_paths:
+        try:
+            # Load the text file with UTF-8 encoding
+            with open(document_path, 'r', encoding='utf-8') as file:
+                document_content = file.read()
 
-    # Define the retriever information
-    retriever_infos = []
+            # Split the document
+            document = TextLoader(document_content).load_and_split()
+            documents.append(document)
+
+            # Create retriever
+            retriever = FAISS.from_documents(document, OpenAIEmbeddings()).as_retriever()
+            retrievers.append(retriever)
+        except Exception as e:
+            #st.error(f"Error loading document {document_path}: {str(e)}")
+            pass
+
+        # Define the retriever information
+        retriever_infos = []
 
     for i, retriever in enumerate(retrievers):
         retriever_info = {
@@ -33,33 +56,51 @@ def initialize_qa_chain(document_paths):
         }
         retriever_infos.append(retriever_info)
 
-    # Create the MultiRetrievalQAChain
-    chain = MultiRetrievalQAChain.from_retrievers(OpenAI(), retriever_infos)
+    # Streamlit input field for API key
+    openai_api_key = st.text_input("Enter your OpenAI API Key", "")
+
+# Set the OpenAI API key
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+
+    # Create the MultiRetrievalQAChain if API key is provided
+    chain = None
+    if openai_api_key:
+        try:
+            # Set the OpenAI API key
+            OpenAI(api_key=openai_api_key)
+            chain = MultiRetrievalQAChain.from_retrievers(OpenAI(), retriever_infos)
+        except Exception as e:
+            st.error("An error occurred while initializing the chain.")
+            st.error(str(e))
 
     return chain
 
 # Streamlit app
 def main():
-    st.title("Question Answering with Langchain and Streamlit")
+    st.title("Question Answering with Documents")
     st.sidebar.title("Settings")
 
     # File paths to your documents
-    document_paths = st.sidebar.text_area("Document Paths (comma-separated)", "/content/30(1-2)+9-17.txt, /content/Kronenfeld-Structuralism-1979.txt").split(",")
+    uploaded_files = st.file_uploader("Upload Documents", accept_multiple_files=True, type=['txt'])
 
-    # Initialize the QA chain
-    chain = initialize_qa_chain([path.strip() for path in document_paths])
+    if uploaded_files:
+        document_paths = [uploaded_file.name for uploaded_file in uploaded_files]
+        chain = initialize_qa_chain(document_paths)
 
-    # Input question
-    question = st.text_input("Ask a question", "")
+        # Display a successful document upload message
+        st.success("Documents successfully uploaded!")
 
-    if st.button("Get Answer"):
-        if question:
-            try:
-                answer = chain.run(question)
-                st.success("Answer: " + answer)
-            except Exception as e:
-                st.error("An error occurred while processing the question.")
-                st.error(str(e))
+        # Input question
+        question = st.text_input("Ask a question", "")
+
+        if st.button("Get Answer"):
+            if chain and question:
+                try:
+                    answer = chain.run(question)
+                    st.success("Answer: " + answer)
+                except Exception as e:
+                    st.error("An error occurred while processing the question.")
+                    st.error(str(e))
 
 if __name__ == "__main__":
     main()
