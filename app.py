1de192f9bf4015825b5b2fb267ae340aed42a436
sheezaW <sheezawaheed1234@gmail.com>
2023-09-28 11:14:22 +0500

    app.py

--- app.py
+++ app.py
@@ -19,43 +19,52 @@ url_input = st.sidebar.text_input("Enter the URL of the document")
 question = st.sidebar.text_input("Ask a question about the document")
 
 if st.sidebar.button("Load Document"):
-    # Load the document from the provided URL
-    loader = WebBaseLoader(url_input)
-    data = loader.load()
+    try:
+        # Check if the URL starts with a scheme (e.g., "http://" or "https://")
+        if not url_input.startswith("http://") and not url_input.startswith("https://"):
+            # If not, add "https://" as the default scheme
+            url_input = "https://" + url_input
 
-    # Split the document into smaller chunks using RecursiveCharacterTextSplitter
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-    all_splits = text_splitter.split_documents(data)
+        # Load the document from the provided URL
+        loader = WebBaseLoader(url_input)
+        data = loader.load()
 
-    # Create embeddings and a document search index
-    embeddings = OpenAIEmbeddings()
-    vectorstore = Chroma.from_documents(documents=all_splits, embedding=embeddings)
+        # Split the document into smaller chunks using RecursiveCharacterTextSplitter
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+        all_splits = text_splitter.split_documents(data)
 
-    # Perform similarity search and retrieve relevant documents
-    retriever_from_llm = MultiQueryRetriever.from_llm(retriever=vectorstore.as_retriever(),
-                                                      llm=ChatOpenAI(temperature=0))
-    unique_docs = retriever_from_llm.get_relevant_documents(query=question)
+        # Create embeddings and a document search index
+        embeddings = OpenAIEmbeddings()
+        vectorstore = Chroma.from_documents(documents=all_splits, embedding=embeddings)
 
-    # Initialize the question-answering chain
-    template = """Use the following pieces of context to answer the question at the end. 
-    If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-    Use three sentences maximum and keep the answer as concise as possible. 
-    Always say "thanks for asking!" at the end of the answer. 
-    {context}
-    Question: {question}
-    Helpful Answer:"""
-    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+        # Perform similarity search and retrieve relevant documents
+        retriever_from_llm = MultiQueryRetriever.from_llm(retriever=vectorstore.as_retriever(),
+                                                          llm=ChatOpenAI(temperature=0))
+        unique_docs = retriever_from_llm.get_relevant_documents(query=question)
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    qa_chain = RetrievalQA.from_chain_type(
-        llm,
-        retriever=vectorstore.as_retriever(),
-        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
-    )
+        # Initialize the question-answering chain
+        template = """Use the following pieces of context to answer the question at the end. 
+        If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+        Use three sentences maximum and keep the answer as concise as possible. 
+        Always say "thanks for asking!" at the end of the answer. 
+        {context}
+        Question: {question}
+        Helpful Answer:"""
+        QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
-    # Get the answer to the question
-    result = qa_chain({"query": question})
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+        qa_chain = RetrievalQA.from_chain_type(
+            llm,
+            retriever=vectorstore.as_retriever(),
+            chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+        )
 
-    # Display the answer
-    st.subheader("Chatbot's Response:")
-    st.write(result["result"])
+        # Get the answer to the question
+        result = qa_chain({"query": question})
+
+        # Display the answer
+        st.subheader("Chatbot's Response:")
+        st.write(result["result"])
+
+    except Exception as e:
+        st.error(f"An error occurred while loading the document: {str(e)}")
