8801581d062bfb935f8dbbe95b1a2d75bc208459
sheezaW <sheezawaheed1234@gmail.com>
2023-09-28 10:57:53 +0500

    app.py

--- app.py
+++ app.py
@@ -1,77 +1,61 @@
 import streamlit as st
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores.elastic_vector_search import ElasticVectorSearch
-from langchain.vectorstores import Chroma
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.memory import ConversationBufferMemory
-from langchain.docstore.document import Document
+from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
 
 # Initialize Streamlit app
 st.title("Document Question Answering App")
 st.sidebar.header("Settings")
 
-# User input for OpenAI key
-openai_key = st.sidebar.text_input("Enter your OpenAI API key")
+# User input for URL
+url_input = st.sidebar.text_input("Enter the URL of the document")
 
-# User input for uploading a text file
-uploaded_file = st.file_uploader("Upload a text file", type=["txt"])
-if uploaded_file is not None:
-    # Use the RecursiveCharacterTextSplitter to split the text
+# User input for questions
+question = st.sidebar.text_input("Ask a question about the document")
+
+if st.sidebar.button("Load Document"):
+    # Load the document from the provided URL
+    loader = WebBaseLoader(url_input)
+    data = loader.load()
+
+    # Split the document into smaller chunks using RecursiveCharacterTextSplitter
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-
-    # Read the content of the uploaded file
-    file_contents = uploaded_file.read()
-    st.sidebar.success("File uploaded successfully!")
-
-    # Split the text into smaller chunks using RecursiveCharacterTextSplitter
-    texts = text_splitter.split_documents(file_contents)
+    all_splits = text_splitter.split_documents(data)
 
     # Create embeddings and a document search index
     embeddings = OpenAIEmbeddings()
-    docsearch = Chroma.from_texts(
-        texts, embeddings, metadatas=[{"source": i} for i in range(len(texts))]
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=embeddings)
+
+    # Perform similarity search and retrieve relevant documents
+    retriever_from_llm = MultiQueryRetriever.from_llm(retriever=vectorstore.as_retriever(),
+                                                      llm=ChatOpenAI(temperature=0))
+    unique_docs = retriever_from_llm.get_relevant_documents(query=question)
+
+    # Initialize the question-answering chain
+    template = """Use the following pieces of context to answer the question at the end. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+    Use three sentences maximum and keep the answer as concise as possible. 
+    Always say "thanks for asking!" at the end of the answer. 
+    {context}
+    Question: {question}
+    Helpful Answer:"""
+    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectorstore.as_retriever(),
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
     )
 
-    # User input for questions
-    question = st.text_input("Ask a question about the document")
+    # Get the answer to the question
+    result = qa_chain({"query": question})
 
-    if st.button("Ask Question"):
-        if not openai_key:
-            st.error("Please enter your OpenAI API key.")
-        elif not question:
-            st.error("Please enter a question.")
-        else:
-            # Load the question-answering chain
-            template = """You are a chatbot having a conversation with a human.
-
-            Given the following extracted parts of a long document and a question, create a final answer.
-
-            {context}
-
-            {chat_history}
-            Human: {human_input}
-            Chatbot:"""
-
-            prompt = PromptTemplate(
-                input_variables=["chat_history", "human_input", "context"], template=template
-            )
-            memory = ConversationBufferMemory(memory_key="chat_history", input_key="human_input")
-            chain = load_qa_chain(
-                OpenAI(temperature=0, api_key=openai_key), chain_type="stuff", memory=memory, prompt=prompt
-            )
-
-            # Perform similarity search and generate a response
-            docs = docsearch.similarity_search(question)
-            response = chain({"input_documents": docs, "human_input": question}, return_only_outputs=True)
-
-            # Display the response
-            st.subheader("Chatbot's Response:")
-            st.write(response)
-
-            # Store the conversation history
-            st.write("Human:", question)
-            st.write("Chatbot:", response)
+    # Display the answer
+    st.subheader("Chatbot's Response:")
+    st.write(result["result"])
